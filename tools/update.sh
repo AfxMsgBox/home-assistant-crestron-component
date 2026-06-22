1cdffd65aadfcf307ea048ef749f1e02747c2b07
AfxMsgBox <85209365+AfxMsgBox@users.noreply.github.com>
2026-06-22 16:17:04 +0800

    Create update.sh

---
+++ tools/update.sh
@@ -0,0 +1,4 @@
+./git-pull.sh https://github.com/AfxMsgBox/home-assistant-crestron-component.git claude/eloquent-hawking-xaiue ./custom_components/crestron
+./git-pull.sh https://github.com/AfxMsgBox/home-assistant-crestron-component.git claude/eloquent-hawking-xaiue ./tools
+chmod +x *.py *.sh
+#ha core restart
